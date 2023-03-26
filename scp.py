e7522d3f1d3e6443c91d8e4ed0cae202885e9fcb
Khalid-Hersafril-Flowcopter <khalid.hersafril@flowcopter.com>
2023-03-26 01:57:05 +0000

    Add logger

--- scp.py
+++ scp.py
@@ -1,10 +1,28 @@
 import pexpect
+import logging
 
 # Config
 ip_addr = "192.168.0.63"
 user = "khalidowlwalid"
 ssh_port = 22
 # Set up the scp command with the appropriate arguments
+
+# Define a logger object with the desired name
+logger = logging.getLogger('my_logger')
+
+# Set the logging level for the logger
+logger.setLevel(logging.INFO)
+
+# Create a console handler and set its logging level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create a formatter and set it on the console handler
+formatter = logging.Formatter('%(asctime)s [%(levelname)s]:  %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add the console handler to the logger
+logger.addHandler(console_handler)
 
 scp_command = f"scp -P {ssh_port} test.txt {user}@{ip_addr}:~/Downloads/"
 
@@ -22,28 +40,28 @@ try:
     child.expect(pexpect.EOF)
 
     # Useful for debugging
-    print(child.before.decode())
+    logging.info(child.before.decode())
 
     file_transfer_flag = "100%" in child.before.decode()
 
     if file_transfer_flag:
-        print("Successfully transferred file..")
+        logger.info("Successfully transferred file..")
     else:
-        print("File not transferred")
+        logger.info("File not transferred")
 
 except pexpect.exceptions.TIMEOUT:
-    print("Connection timed out")
-    print("Check if your password has been correctly set")
+    logger.error("Connection timed out")
+    logger.error("Check if your password has been correctly set")
 
 except pexpect.exceptions.EOF:
     args = child.args
     before = str(child.before)
-    print(before)
+    logger.error(before)
 
     if "connection refused" in before.lower():
-        print(f"Connect to host {ip_addr} at port {ssh_port}: Connection refused. Please check if you have enabled your port is enabled.")
-    print("SCP command exited unexpectedly")
+        logger.error(f"Connect to host {ip_addr} at port {ssh_port}: Connection refused. Please check if you have enabled your port is enabled.")
+    logger.error("SCP command exited unexpectedly")
 
 except pexpect.exceptions.ExceptionPexpect as e:
-    print(f"An exception occurred: {str(e)}")
+    logger.error(f"An exception occurred: {str(e)}")
 
