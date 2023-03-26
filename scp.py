9f5149ba53bc24c7164b9c96d797c3b8dda6a300
Khalid-Hersafril-Flowcopter <khalid.hersafril@flowcopter.com>
2023-03-26 06:39:44 +0000

    Working!

--- scp.py
+++ scp.py
@@ -1,15 +1,15 @@
 import pexpect
 import logging
 import time
+import getpass
 
 # Config
 ip_addr = "192.168.0.63"
 user = "khalidowlwalid"
 ssh_port = 22
 logger_name = 'throttle_callibrator'
-user_password = 'jellybean123'
-user_path = '~/Documents/'
-file_name = 'test.txt'
+saved_file_path = '~/Documents/'
+file_name = 'alia.txt'
 
 # Set up the scp command with the appropriate arguments
 
@@ -19,7 +19,7 @@ logger = logging.getLogger(logger_name)
 logging.getLogger().setLevel(logging.NOTSET)
 
 # Create a file handler and set its logging level
-file_handler = logging.FileHandler('my_log_file.log')
+file_handler = logging.FileHandler('log/automated_throttle_callibration.log')
 
 # Create a formatter and set it on the file handler
 formatter = logging.Formatter(f'{logger.name}: [%(asctime)s][%(levelname)s]: %(message)s')
@@ -34,90 +34,89 @@ console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
-scp_command = f"scp -P {ssh_port} {file_name} {user}@{ip_addr}:{user_path}"
-
-try:
-    # Start the scp command using pexpect
-    child = pexpect.spawn(scp_command)
-
-    # Wait for the password prompt
-    child.expect("password:")
-
-    # Enter password
-    child.sendline(user_password)
+scp_command = f"scp -P {ssh_port} {file_name} {user}@{ip_addr}:{saved_file_path}"
 
 
-    index_flag = child.expect([file_name, "Permission denied", "password:"])
+# Start the scp command using pexpect
+child = pexpect.spawn(scp_command)
 
-    if index_flag == 0:
-        logger.info("Login successful!")
+login_success_flag = False
 
-    elif index_flag == 1 or index_flag == 2:
-        logger.error("Permission denied. Wrong password. Please try again")
-        raise KeyboardInterrupt
+failed_attempts = 0
 
-    # Check if user inputs wrong password
-    # If not, then child expects to see the shell prompt, hence the $
-    # index_flag = child.expect(["Permission denied", "password:"], timeout=5)
+while not login_success_flag:
+    try:
 
-#    if index_flag == 0:
-#        logger.info("Permission denied. Check your password and try again")
+        # Wait for the password prompt
+        child.expect("password:")
 
-        # Raise keyboard interrupt to avoid waiting time
-#        raise KeyboardInterrupt
-#    elif index_flag == 1:
-#        logger.info("Login successful!")
-#    else:
-#        logger.info("Timeout reached: Login unsuccesful.")
-#        raise pexpect.exceptions.TIMEOUT
+        # Enter password
+        user_password = getpass.getpass(f"{user}@{ip_addr}'s password:")
+        child.sendline(user_password)
 
-    """
-    -----------------------------------------------
-    Example prompt response during successful login
-    -----------------------------------------------
-    Password:
-    sending incremental file list
-    example.txt
-          7 100%    0.00kB/s    0:00:00 (xfer#1, to-check=0/1)
+        index_flag = child.expect([file_name, "Permission denied", "password:"])
 
-    sent 28 bytes  received 33 bytes  122.00 bytes/sec
-    total size is 7  speedup is 0.08
+        if index_flag == 0:
+            logger.info("Login successful!")
+            login_success_flag = True
+        elif index_flag == 1 or index_flag == 2:
+            logger.error("Permission denied. Wrong password. Please try again")
+            failed_attempts += 1
 
-    EOF
-    """
+            if failed_attempts == 3:
+                logger.error(f"Maximum number of attempts reached. File will not be saved to {user}@{ip_addr}'s {saved_file_path}")
+                logger.info(f"User will need to download the file locally from the Raspberry Pi. The full path of the file can be found in {file_name}.")
+                raise KeyboardInterrupt
 
-    # Wait for the transfer to complete
-    child.expect(pexpect.EOF)
+            continue
+        """
+        -----------------------------------------------
+        Example prompt response during successful login
+        -----------------------------------------------
+        Password:
+        sending incremental file list
+        example.txt
+              7 100%    0.00kB/s    0:00:00 (xfer#1, to-check=0/1)
 
-    # Useful for debugging
-    logger.info(f"Output: {child.before.decode()}")
+        sent 28 bytes  received 33 bytes  122.00 bytes/sec
+        total size is 7  speedup is 0.08
 
-    # Check if file is completely transferred or not
-    file_transfer_flag = "100%" in child.before.decode()
+        EOF
+        """
 
-    if file_transfer_flag:
-        logger.info("Successfully transferred file..")
-        logger.info(f"File saved in {user_path}")
-    else:
-        logger.warning("File not transferred")
+        # Wait for the transfer to complete
+        child.expect(pexpect.EOF)
 
-# Check for timeout
-except pexpect.exceptions.TIMEOUT:
-    logger.error("Connection timed out")
-    logger.error("Check if your password has been correctly set")
+        # Useful for debugging
+        logger.info(f"Output: {child.before.decode()}")
 
-# Check if for any reason, something breaks the command
-except pexpect.exceptions.EOF:
-    before = str(child.before)
-    print(child)
+        # Check if file is completely transferred or not
+        file_transfer_flag = "100%" in child.before.decode()
 
-    if "connection refused" in before.lower():
-        logger.error(f"Connect to host {ip_addr} at port {ssh_port}: Connection refused. Please check if you have enabled your port is enabled.")
-    logger.error("SCP command exited unexpectedly")
+        if file_transfer_flag:
+            logger.info("Successfully transferred file..")
+            logger.info(f"File saved in {saved_file_path}")
+        else:
+            logger.warning("File not transferred")
 
-except pexpect.exceptions.ExceptionPexpect as e:
-    logger.error(f"An exception occurred: {str(e)}")
+    # Check for timeout
+    except pexpect.exceptions.TIMEOUT:
+        logger.error("Connection timed out")
+        logger.error("Check if your password has been correctly set")
 
-# Allow user to exit with keyboard
-except KeyboardInterrupt:
-    logger.info("Keyboard Interrupt. Shutting down..")
+    # Check if for any reason, something breaks the command
+    except pexpect.exceptions.EOF:
+        before = str(child.before)
+        print(child)
+
+        if "connection refused" in before.lower():
+            logger.error(f"Connect to host {ip_addr} at port {ssh_port}: Connection refused. Please check if you have enabled your port is enabled.")
+        logger.error("SCP command exited unexpectedly")
+
+    except pexpect.exceptions.ExceptionPexpect as e:
+        logger.error(f"An exception occurred: {str(e)}")
+
+    # Allow user to exit with keyboard
+    except KeyboardInterrupt:
+        login_success_flag = True
+        logger.info("Keyboard Interrupt. Shutting down..")
